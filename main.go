bda542315263a431816958b69fa036032319dc44
Ken Duigraha Putra <kenduigraha@gmail.com>
2025-08-27 10:21:48 +0700

    chore: add endpoint login to check user from redis.

--- main.go
+++ main.go
@@ -1,38 +1,96 @@
-import (
-    "context"
-    "fmt"
+package main
 
-    "github.com/redis/go-redis/v9"
+import (
+	"crypto/sha1"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/redis/go-redis/v9"
+	"github.com/gofiber/fiber/v3"
+	"golang.org/x/net/context"
 )
 
 var ctx = context.Background()
 
-func ExampleClient() {
-    rdb := redis.NewClient(&redis.Options{
-        Addr:     "localhost:6379",
-        Password: "", // no password set
-        DB:       0,  // use default DB
-    })
+// Struct user sesuai format Redis
+type User struct {
+	RealName string `json:"realname"`
+	Email    string `json:"email"`
+	Password string `json:"password"` // hashed sha1
+}
 
-    err := rdb.Set(ctx, "key", "value", 0).Err()
-    if err != nil {
-        panic(err)
-    }
+type LoginRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
 
-    val, err := rdb.Get(ctx, "key").Result()
-    if err != nil {
-        panic(err)
-    }
-    fmt.Println("key", val)
+// Fungsi hash password dengan SHA1
+func sha1Hash(s string) string {
+	h := sha1.New()
+	h.Write([]byte(s))
+	return hex.EncodeToString(h.Sum(nil))
+}
 
-    val2, err := rdb.Get(ctx, "key2").Result()
-    if err == redis.Nil {
-        fmt.Println("key2 does not exist")
-    } else if err != nil {
-        panic(err)
-    } else {
-        fmt.Println("key2", val2)
-    }
-    // Output: key value
-    // key2 does not exist
+func main() {
+	// Inisialisasi Redis v9
+	rdb := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+		DB:   0,
+	})
+
+	app := fiber.New()
+
+	// Define a route for the GET method on the root path '/'
+  app.Get("/", func(c fiber.Ctx) error {
+      // Send a string response to the client
+      return c.SendString("Hello, World 👋!")
+  })
+	
+	app.Post("/login", func(c fiber.Ctx) error {
+		var req LoginRequest
+		if err := c.Bind().Body(&req); err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"error": "Invalid request",
+			})
+		}
+
+		// check key in Redis
+		key := fmt.Sprintf("login_%s", req.Username)
+		val, err := rdb.Get(ctx, key).Result()
+
+		if err == redis.Nil {
+			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+				"error": "User not found",
+			})
+		} else if err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": "Redis error",
+			})
+		}
+
+		var user User
+		if err := json.Unmarshal([]byte(val), &user); err != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": "Invalid user data",
+			})
+		}
+
+		// check password
+		if user.Password != sha1Hash(req.Password) {
+			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+				"error": "Invalid username or password",
+			})
+		}
+
+		return c.JSON(fiber.Map{
+			"message":  "Login successful",
+			"realname": user.RealName,
+			"email":    user.Email,
+		})
+	})
+
+	// Run server
+	log.Fatal(app.Listen(":3000"))
 }
